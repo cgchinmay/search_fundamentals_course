9a4e9c6d88f484f123963c16ebdb43c8e391bb56
cgchinmay <chinmay5j@gmail.com>
2023-02-20 05:48:36 +0000

    level 3

--- week2/search.py
+++ week2/search.py
@@ -9,6 +9,8 @@ from week2.opensearch import get_opensearch
 
 import week2.utilities.query_utils as qu
 
+from week2.utilities.query_utils import add_spelling_suggestions
+
 bp = Blueprint('search', __name__, url_prefix='/search')
 
 
@@ -57,15 +59,32 @@ def process_filters(filters_input):
 
 @bp.route('/autocomplete', methods=['GET'])
 def autocomplete():
+    opensearch = get_opensearch()
     results = {}
     if request.method == 'GET':  # a query has been submitted
         prefix = request.args.get("prefix")
         print(f"Prefix: {prefix}")
+        indexName = "bbuy_products"
         if prefix is not None:
             type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
             ##### W2, L3, S1
             search_response = None
             print("TODO: implement autocomplete AND instant search")
+            query_obj = {
+                    "suggest": {
+                    "autocomplete": {
+                            "prefix": prefix,
+                            "completion":{
+                                "field":"suggest",
+                                "skip_duplicates": True
+                            }
+                        }
+                    }
+                }
+            if type == "queries":
+                indexName = "bbuy_queries"
+            
+            search_response = opensearch.search(body=query_obj, index=indexName)
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
     print(f"Results: {results}")
@@ -125,6 +144,7 @@ def query():
 
         ##### W2, L2, S2
         add_spelling_suggestions(query_obj, user_query)
+        print(query_obj)
     else:
         query_obj = qu.create_query("*", "", [], sort, sortDir, size=100)
 
